24a31e620a66aa6cbfc94084b4bc403b75fa4d74
sergo book <sergo@book.ru>
2024-06-14 22:48:21 +0300

    lesson1:task1 is very done

--- lesson1/task1.py
+++ lesson1/task1.py
@@ -56,8 +56,8 @@ iter **= 5  # iter = iter ** 5"""
 # print(a)
 # a = 1 != 2
 # print(a)
-a = 'qwe'
-b = 'qwe'
-print(a == b)
-a = 1 < 3 < 5 < 10
-print(a)
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+# a = 1 < 3 < 5 < 10
+# print(a)
