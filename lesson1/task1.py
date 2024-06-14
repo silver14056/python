5b8e41262c6353fd27946b7a71000b92594962ba
sergo book <sergo@book.ru>
2024-06-14 21:29:43 +0300

    lesson1:task1 is done

--- lesson1/task1.py
+++ lesson1/task1.py
@@ -4,7 +4,60 @@
 print(type(n))
 print(n)"""
 
-a = 5
+"""a = 5
 b = 5.89
 c = 'hello'
 print(a, ' - ', b,' - ', c)
+print(f"{a} - {b} _ {c}")
+print("{} - {} - {}". format(a, b, c))"""
+
+"""print('Введите первое число: ')
+a = input()
+b = input('Введите второе число: ')
+print(a, ' + ', b, '=', a + b)"""
+
+"""c = 5.89
+print(c)
+print(type(c))
+c = str(c)
+print(c + 'a')
+print(type(c))"""
+
+"""c = 1
+print(c)
+print(type(c))
+c = bool(c)
+print(c)
+print(type(c))"""
+
+"""print('Введите первое число: ')
+a = int(input())
+b = int(input('Введите второе число: '))
+print(a, ' + ', b, '=', a + b)"""
+
+"""a = 5.89956
+b = 6.556551
+print(round(a * b, 3))"""
+
+"""iter = 2
+iter += 3  # iter = iter + 3
+iter -= 4  # iter = iter - 4
+iter *= 5  # iter = iter * 5
+iter /= 5  # iter = iter / 5
+iter //= 5  # iter = iter // 5
+iter %= 5  # iter = iter % 5
+iter **= 5  # iter = iter ** 5"""
+
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 == 2
+# print(a)
+# a = 1 != 2
+# print(a)
+a = 'qwe'
+b = 'qwe'
+print(a == b)
+a = 1 < 3 < 5 < 10
+print(a)
