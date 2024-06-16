235d9aab347a687a45a18b27c9c983a737038033
Serg Litov <Serg@litov.com>
2024-06-16 09:02:14 +0300

    lesson1:task2 is done

--- lesson1/task2.py
+++ lesson1/task2.py
@@ -21,7 +21,7 @@ else:
 print(i)"""
 
 # Метод флажка
-n = int(input())
+"""n = int(input())
 flag = True
 i = 2
 while flag:
@@ -31,4 +31,37 @@ while flag:
     elif i > n // 2:  # делитель числа не может превышать введенное число, деленное на 2
         print(n)
         flag = False
-    i += 1
+    i += 1"""
+
+# for и range
+
+"""line = ""
+for i in range(3):
+    line = ""
+    for j in range(5):
+        line += "*"
+    print(line)"""
+
+#Строки
+"""text = 'СъЕШЬ ещё этих МяГкИх французских булок'
+print(len(text))
+print('ещё' in text)
+print(text.lower())
+print(text.upper())
+print(text.replace('ещё', 'ЕЩЁ'))"""
+
+#срез строк
+text = 'СъЕШЬ ещё этих МяГкИх французских булок'
+print(text[0])                              # с
+print(text[1])                              # ъ
+print(text[len(text) - 1])                  # к
+print(text[-5])                             # б
+print(text[:])                              # СъЕШЬ ещё этих МяГкИх французских булок
+print(text[:2])                             # съ
+print(text[len(text) - 2:])                 # ок
+print(text[2:9])                            # ЕШЬ ещё
+print(text[6:-18])                          # ещё этих МяГкИх
+print(text[0:len(text):6])                  # Сеикакл
+print(text[::6])                            # Сеикакл
+text = text[2:9] + text[-5] + text[:2]      # ...
+print(text)                                 # ЕШЬ ещёбСъ
