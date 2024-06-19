f8ac00ae5145ac9bcffbea2fce63d263cb8b859e
Serg Litov <Serg@litov.com>
2024-06-19 10:00:45 +0300

    start lesson2:task2

---
+++ lesson2/task2.py
@@ -0,0 +1,13 @@
+
+"""list_1 = [1, 5]
+print(list_1)
+list_1.append(8)
+print(list_1)
+list_1.append(85)
+print(list_1)
+"""
+
+list_1 = []
+for i in range(5):
+    list_1.append(i)
+    print(list_1)
