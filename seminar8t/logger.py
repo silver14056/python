15963e6afcdd43e40f42fbce8505e0f4138d4982
Serg Litov <Serg@litov.com>
2024-07-14 22:44:26 +0300

    seminar8t with DZ v1.0

--- seminar8t/logger.py
+++ seminar8t/logger.py
@@ -21,7 +21,7 @@ def input_data():
             f.write(f"{name}\n{surname}\n{phone}\n{address}\n\n")
     elif var == 2:
         with open('data_second_variant.csv', 'a', encoding='utf-8') as f:
-            f.write(f"{name};{surname};{phone};{address}\n\n")
+            f.write(f"{name};{surname};{phone};{address}\n")
 
 
 def print_data():
@@ -29,14 +29,78 @@ def print_data():
     with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
         data_first = f.readlines()
         data_first_list = []
-        j = 0
+        j = -1
         for i in range(len(data_first)):
             if data_first[i] == '\n' or i == len(data_first) - 1:
+                j += 1
                 data_first_list.append(''.join(data_first[j:i + 1]))
                 j = i
         print(''.join(data_first_list))
-
     print('Вывожу данные из 2 файла: \n')
     with open('data_second_variant.csv', 'r', encoding='utf-8') as f:
         data_second = f.readlines()
         print(*data_second)
+
+
+def change_data():
+    var = int(input("В какую телефонную книгу будем вносить изменения: "))
+    while var != 1 and var != 2:
+        print("Неправильный ввод")
+        var = int(input('Введите правильное число: '))
+    key = int(input("Введите номер записи которую нужно изменить: "))
+    name = name_data()
+    surname = surname_data()
+    phone = phone_data()
+    address = address_data()
+    if var == 1:
+        with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
+            data_first = f.readlines()
+            data_first_list = []
+            j = -1
+            for i in range(len(data_first)):
+                if data_first[i] == '\n' or i == len(data_first) - 1:
+                    j += 1
+                    data_first_list.append(''.join(data_first[j:i + 1]))
+                    j = i
+            data_first_list[key-1] = f"{name}\n{surname}\n{phone}\n{address}\n\n"
+            print((''.join(data_first_list)))
+        with open('data_first_variant.csv', 'w', encoding='utf-8') as f:
+            f.write(''.join(data_first_list))
+    elif var == 2:
+        with open('data_second_variant.csv', 'r', encoding='utf-8') as f:
+            data_second = f.readlines()
+            print(data_second)
+            data_second[key-1] = f"{name};{surname};{phone};{address}\n"
+            print(*data_second)
+        with open('data_second_variant.csv', 'w', encoding='utf-8') as f:
+            f.write(''.join(data_second))
+
+
+def delete_data():
+    var = int(input("В какую телефонную книгу будем вносить изменения: "))
+    while var != 1 and var != 2:
+        print("Неправильный ввод")
+        var = int(input('Введите правильное число: '))
+    key = int(input("Введите номер записи которую нужно удалить: "))
+    if var == 1:
+        with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
+            data_first = f.readlines()
+            data_first_list = []
+            j = -1
+            for i in range(len(data_first)):
+                if data_first[i] == '\n' or i == len(data_first) - 1:
+                    j += 1
+                    data_first_list.append(''.join(data_first[j:i + 1]))
+                    j = i
+            data_first_list.pop(key-1)
+            print((''.join(data_first_list)))
+        with open('data_first_variant.csv', 'w', encoding='utf-8') as f:
+            f.write(''.join(data_first_list))
+    elif var == 2:
+        with open('data_second_variant.csv', 'r', encoding='utf-8') as f:
+            data_second = f.readlines()
+            print(data_second)
+            data_second.pop(key-1)
+            print(*data_second)
+        with open('data_second_variant.csv', 'w', encoding='utf-8') as f:
+            f.write(''.join(data_second))
--- seminar8t/ui.py
+++ seminar8t/ui.py
@@ -1,10 +1,12 @@
-from logger import input_data, print_data
+from logger import input_data, print_data, change_data, delete_data
+
 
 def interface():
-    print("Добрый день! Вы попали на специальный бот справочник от GeekBrains! \n 1 - Запись данных\n 2 - Вывод данных")
-    command = int(input('Ввелите число: '))
+    print("Добрый день! Вы попали на специальный бот справочник от GeekBrains!\n1 - Запись\n2 - Вывод\n"
+          "3 - Изменение\n4 - Удаление")
+    command = int(input('Введите число: '))
 
-    while command != 1 and command != 2:
+    while command != 1 and command != 2 and command != 3 and command != 4:
         print("Неправильный ввод")
         command = int(input('Ввелите число: '))
 
@@ -12,6 +14,7 @@ def interface():
         input_data()
     elif command == 2:
         print_data()
-
-
-
+    elif command == 3:
+        change_data()
+    elif command == 4:
+        delete_data()
