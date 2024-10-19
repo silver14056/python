71231411eafced9a3021b8e7bbed0d2882febebb
sergo book <sergo@book.ru>
2024-10-19 11:13:54 +0300

    Modification:read(add "reading by id")

--- final_attestation/create.py
+++ final_attestation/create.py
@@ -2,9 +2,10 @@ from datetime import datetime
 
 
 def create_note():
+    print("--------------------------------------------------")
     id_note = 1
-    title = input("Введите заголовок заметки ")
-    note_body = input("Напишите вашу заметку ")
+    title = input("Введите заголовок заметки:")
+    note_body = input("Напишите вашу заметку:")
     date_time_create = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M")
     with open("notes.csv", "a", encoding="utf-8") as f:
         f.write(f"{id_note};{title};{note_body};{date_time_create}\n")
--- final_attestation/read.py
+++ final_attestation/read.py
@@ -1,11 +1,38 @@
 import csv
+from check_id import check_id
 
 
 def read_note():
-    with open("notes.csv", "r", encoding="utf-8") as f:
-        data = csv.reader(f, delimiter=";", skipinitialspace=True)
-        print()
-        for row in data:
-            print(
-                f"Заметка №{row[0]}\nЗаголовок: {row[1]}\nЗаметка: {row[2]}\nДата: {row[3]}\n"
-            )
+    print("--------------------------------------------------")
+    print(
+        "Вывести все заметки - 1\nвывод заметки по номеру - 2"
+    )
+    print("--------------------------------------------------")
+    command = int(input("Введите номер операции:"))
+    while command not in [1, 2]:
+        print("--------------------------------------------------")
+        print("Неправильный ввод. Попробуйте еще раз!")
+        command = int(input("Введите номер операции:"))
+
+    if command == 1:
+        with open("notes.csv", "r", encoding="utf-8") as f:
+            data = csv.reader(f, delimiter=";", skipinitialspace=True)
+            print()
+            for row in data:
+                print(
+                    f"Заметка №{row[0]}\nЗаголовок: {row[1]}\nЗаметка: {row[2]}\nДата: {row[3]}\n"
+                )
+    elif command == 2:
+        print("--------------------------------------------------")
+        num_id = int(input("Введите номер заметки для просмотра:"))
+        if check_id(num_id):
+            with open("notes.csv", "r", encoding="utf-8") as f:
+                data = csv.reader(f, delimiter=";", skipinitialspace=True)
+                for row in data:
+                    if int(row[0]) == num_id:
+                        print(
+                            f"\nЗаметка №{row[0]}\nЗаголовок: {row[1]}\nЗаметка: {row[2]}\nДата: {row[3]}\n"
+                        )
+        else:
+            print("--------------------------------------------------")
+            print("Заметки с таким номером нет!")
